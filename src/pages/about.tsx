f668cbc8d418b7c5ca97f405fdb244bbe0305096
Eric Tran <erictran88@gmail.com>
2022-08-15 22:14:11 -0400

    fix: hello world

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -3,6 +3,17 @@ import { Main } from '@/templates/Main';
 
 const About = () => (
   <Main meta={<Meta title="Lorem ipsum" description="Lorem ipsum" />}>
+    Powered with{' '}
+    <span role="img" aria-label="Love">
+      ♥
+    </span>{' '}
+    by <a href="https://creativedesignsguru.com">CreativeDesignsGuru</a>
+    {/*
+     * PLEASE READ THIS SECTION
+     * We'll really appreciate if you could have a link to our website
+     * The link doesn't need to appear on every pages, one link on one page is enough.
+     * Thank you for your support it'll mean a lot for us.
+     */}
     <p>
       Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione fuga
       recusandae quidem. Quaerat molestiae blanditiis doloremque possimus labore
