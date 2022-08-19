c05905698ac78811703d54322075dc1d80595a99
Eric Tran <erictran88@gmail.com>
2022-08-19 18:13:09 -0400

    feat: add ingredients image

--- src/pages/slide1.tsx
+++ src/pages/slide1.tsx
@@ -1,34 +1,46 @@
+import { useRouter } from 'next/router';
+
 import { Meta } from '@/layouts/Meta';
 import { Main } from '@/templates/Main';
 
-const Slide1 = () => (
-  <Main meta={<Meta title="Slide 1" description="Slide 1" />}>
-    <div style={{ height: '640px', width: '360px' }} className="bg-white">
-      <div> Project Cooking</div>
-      <div>
-        <p>whats in your kitchen?</p>
-        <input type="text" className="border-2 border-solid border-stone-800" />
-        <button className="border-2 border-solid border-stone-800">Add</button>
-      </div>
-      <hr className="h-1 bg-slate-800" />
-      <div>
-        <div style={{ height: '120px', width: '120px' }}>
-          {'PICTURE PLACEHOLDER'}
+const Slide1 = () => {
+  const router = useRouter();
+  return (
+    <Main meta={<Meta title="Slide 1" description="Slide 1" />}>
+      <div style={{ height: '640px', width: '360px' }} className="bg-white">
+        <div> Project Cooking</div>
+        <div>
+          <p>whats in your kitchen?</p>
+          <input
+            type="text"
+            className="border-2 border-solid border-stone-800"
+          />
+          <button className="border-2 border-solid border-stone-800">
+            Add
+          </button>
+        </div>
+        <hr className="h-1 bg-slate-800" />
+        <div>
+          <img
+            src={`${router.basePath}/assets/images/ing_placeholder.svg`}
+            alt="Ingredients Place Holder Image"
+          />
+
+          <div>
+            {' '}
+            <p>
+              Your ingredients will <br></br> appear here.
+            </p>
+          </div>
         </div>
         <div>
-          {' '}
-          <p>
-            Your ingredients will <br></br> appear here.
-          </p>
+          <button className="border-2 border-solid border-stone-800">
+            {'Find Recipes'}
+          </button>
         </div>
       </div>
-      <div>
-        <button className="border-2 border-solid border-stone-800">
-          {'Find Recipes'}
-        </button>
-      </div>
-    </div>
-  </Main>
-);
+    </Main>
+  );
+};
 
 export default Slide1;
