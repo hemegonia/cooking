a8c1acef398a5c1f36787e47351f433ed2928670
Eric Tran <etran@kmbs.konicaminolta.us>
2022-09-18 00:59:26 -0400

    feat: add icons to header and style receipe cards

--- src/components/cookingApp/header.tsx
+++ src/components/cookingApp/header.tsx
@@ -1,7 +1,22 @@
+import { useRouter } from 'next/router';
+
 const Header = () => {
+  const router = useRouter();
   return (
-    <div className="flex h-24 items-center justify-center bg-primary text-white">
+    <div className="flex h-24 items-center justify-between bg-primary p-8 text-white">
+      <button onClick={router.back}>
+        <img
+          src={`${router.basePath}/assets/images/back_arrow.svg`}
+          alt="Ingredients Place Holder Image"
+        />
+      </button>
       <h1>Project Cooking</h1>
+      <button>
+        <img
+          src={`${router.basePath}/assets/images/ellipsis.svg`}
+          alt="Ingredients Place Holder Image"
+        />
+      </button>
     </div>
   );
 };
